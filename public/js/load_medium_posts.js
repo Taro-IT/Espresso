76b60ae97bef8015151c3cb8c471cd3164c43d6c
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-10-15 00:25:42 -0600

    feat: posts layout

--- public/js/load_medium_posts.js
+++ public/js/load_medium_posts.js
@@ -6,49 +6,22 @@ fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@sab
       const posts = data.items.filter(item => item.categories.length > 0) 
 
       // Format post values as html
-      let postsHtml = '';
-      posts.forEach((item) => {
-         // Remove image caption from content
-         figureStart = item.content.search("<figure>");
-         figureEnd = item.content.search("</figure>");
-         if(figureStart != -1 && figureEnd != -1) {
-            item.content = item.content.substring(0, figureStart) + item.content.substring(figureEnd + 9);
-         }
-
-         postsHtml += `
-         <div class="post">
-            <a href="${item.link}">
-               <img src="${item.thumbnail}" class="post_img"></img>
-               <div class="post_content">
-                  <div class="blog_preview">
-                     <h2 class="post_title">${item.title}</h2>
-                     <p class="post_intro">${shortenText(toText(item.content), 100) + "..."}</p>
-                  </div>
-                  <hr>
-                  <div class="post_info">
-                     <p class="post_author">${item.author}</p>
-                     <p class="post_date">${shortenText(item.pubDate, 10)}</p>
-                  </div>
-               </div>
-            <a/>
-         </div>`;
-      }) 
-      document.querySelector('.posts_container').innerHTML = postsHtml;
+      for(let i = 1; i <= 4; i++) {
+         const post = posts[i];
+         const postHtml = getPostHtml(post.link, post.thumbnail, post.title, post.author)
+         document.querySelector(`[data-medium-post-id='${i}']`).innerHTML = postHtml;
+      }   
    })
    .catch((err => console.log(err)));
 
-// Remove html tags from a string
-function toText(node) {
-   let tag = document.createElement('div')
-   tag.innerHTML = node;
-   node = tag.innerText;
-   return node;
-}
-
-// Trim the end of a string to make it shorter or equal to a length
-function shortenText(text, maxLength) {
-   if(text.length > maxLength) {
-      return text.substring(0, maxLength);
-   }
-   return text;
+function getPostHtml(link, thumbnail, title, author) {
+   postsHtml = `
+      <div class="medium_post">
+         <a href="${link}">
+            <img src="${thumbnail}" class="medium_post_img rounded-lg"></img>
+            <h2 class="medium_post_title text-center ">${title}</h2>
+         <a/>
+         <p class="medium_post_author text-center ">${author}</p>
+      </div>`;
+   return postsHtml;
 }
---
+++ resources/views/medium.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medium posts</title>
+    <!-- Boostrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Custom css -->
+    <link rel="stylesheet" href="{{ asset('/css/medium_posts_styles.css') }}">
+</head>
+<body>
+    <!-- Mediums posts section -->
+    <section>
+        <h1 class="text-center">Acercando la locura</h1>
+        <p class="text-center">Explora nuestra colección de articulos de divulgación, producciones literarias, producciones narrativas, proyectos e historias de vida de nuestra comunidad</p>   
+        <div class="container">
+            <div class="row">
+                <div class="col-lg" data-medium-post-id="1"></div>
+                <div class="col-lg">
+                    <div data-medium-post-id="2"></div>
+                    <div class="row">
+                        <div class="col" data-medium-post-id="3"></div>
+                        <div class="col" data-medium-post-id="4"></div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-outline-info">Descubre mas artículos</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Load medium posts -->
+    <script src="{{ asset('/js/load_medium_posts.js') }}"></script>
+
+    <!-- Bootstrap js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+</html>
