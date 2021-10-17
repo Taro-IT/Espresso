909d2b2e614b7af848c4ebfc070ff436b2f61721
Jorge Andres <A01209043@itesm.mx>
2021-10-17 15:37:17 -0500

    Model & Controller for basic infomation of the web page

--- app/Models/PageInfo.php
+++ app/Models/PageInfo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Page extends Model
+class PageInfo extends Model
 {
     use HasFactory;
 }
