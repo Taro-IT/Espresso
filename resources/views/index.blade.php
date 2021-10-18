b567b6cfa1c4e2d2ceafe72cd735914bc2a40859
JoseCarlosPa <josecarlospas1@gmail.com>
2021-10-18 16:37:29 -0500

    feat: Se agregan los MetaTags , Favicons y estructura general del app.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,13 @@
-{{-- extends indica que la vista hereda de otra y va a inyectar contenidos de ella--}}
 @extends('layouts.app')
-{{-- section indica lo que se va a ver en la página --}}
+
+
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
+
     <ul>
         <li><a href="{{ url('/bot_test') }}">Test de bot</a></li>
         <li><a href="{{ url('/facebook_test') }}">Test de facebook</a></li>
         <li><a href="{{ url('/medium_test') }}">Test de Medium</a></li>
         <li><a href="{{ url('/paypal_test') }}">Test de PayPal</a></li>
     </ul>
-</body>
-</html>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Meta TAGS-->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name=”robots” content=”noindex,follow”>
+    <meta property=”og:type” content=”https://casademediocamino.com/”/>
+    <meta property=”og:title” content=”Casa Medio Camino Loohl”/>
+
+    <!-- Descripción y palabras calves para manejo de SEO-->
+    <meta name="description" content="Casa de Medio Camino, es un modelo de comunidad terapéutica que se origina
+          como parte de una propuesta integral al tratamiento de salud mental, para personas que han sido diagnosticadas
+          por su psiquiatra de cabecera con alguna enfermedad mental. Siendo un punto intermedio entre la
+          hospitalización y la casa de origen del paciente.">
+    <meta name="keywords"
+          content="Pacientes, Ayuda, ayuda, pacientes, alternativa, psicólogos,Psicólogos,salud mental,necesidades,
+          psiquiatra,afectivos,">
+
+    <!-- Favicon de Pagina web-->
+    <link rel="icon" href="{{asset('images/logo.jpg')}}" type="image/x-icon">
+
     <!-- Boostrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Materialize Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Loohl</title>
+
+    <!--CSS Personalizado por blade-->
+    <link rel="stylesheet" href="{{asset('css/main.css')}}" crossorigin="anonymous">
+    @stack('css')
+
+    <!-- Titulo-->
+    <title>@yield('title','Casa Medio camino Loohl | Inicio')</title>
+
 </head>
+
 <body>
-    <x-header>
-    </x-header>
-        <!-- Page Content -->
-        <main>
-            @yield('content')
-        </main>  
-    <x-footer>
-    </x-footer>
+    <main>
+        @yield('content')
+    </main>
 </body>
+
 <!-- Boostrap 5 JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+<!-- JS Personalizado-->
+<script rel="script" src="{{asset('js/main.js')}}" crossorigin="anonymous"></script>
+@stack('scripts')
+
 </html>
