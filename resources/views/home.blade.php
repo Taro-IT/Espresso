f4fd79ee7f8fa738db412a6bfa23552fd08382fd
JoseCarlosPa <josecarlospas1@gmail.com>
2021-10-25 16:38:23 -0500

    feature: Se agrega el item "Cerrar Sesion"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,15 @@
             </div>
         </div>
     </div>
+
+    <a class="" href="{{ route('logout') }}"
+       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+        <i class="icon_key_alt"></i>Cerrar Sesion
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
 </div>
+
 @endsection
