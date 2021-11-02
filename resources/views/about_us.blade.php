3f5a64e66b787029ca016e3fd57e57db2ed6d5aa
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-11-02 16:30:13 -0700

    fix: about us aligment

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -7,26 +7,26 @@
     <img class="d-block w-100" src="{{asset('images/slider-1.png')}}" alt="First slide">
 </section>
 
-<!-- =======  WHO WE ARE SECTION ======= -->
 <div class="container">
-<section id="quienes-somos">
-    <div class="row justify-content-center">
-        <h1 class="section-header" data-aos="fade-up" data-aos-delay="200">
-            <span class="underline">&nbsp;&nbsp;¿QUIÉNES SOMOS?&nbsp;&nbsp;</span>
-        </h1>
-        <p class="subtitle text-center" data-aos="fade-up" data-aos-delay="400">
-            Somos un grupo de psicólogos con experiencia en las áreas de trabajo social, 
-            clínico y de investigación sobre el trabajo y seguimiento de pacientes con 
-            experiencias psiquiátricas quienes tomamos iniciativa frente a la realidad 
-            social en México de las personas que han sido diagnosticadas con padecimientos 
-            psiquiátricos o que han tenido dificultades para reintegrarse a sus actividades 
-            sociales, familiares y ocupacionales después de una crisis. Con ello buscamos 
-            brindar un espacio de integración, interacción y de inclusión en un ambiente 
-            de aceptación y comunidad.
-        </p>
-    </div>
-    <div class="container" data-aos="fade-up" data-aos-delay="600">
-        <div class="row justify-content-center">
+    <!-- =======  WHO WE ARE SECTION ======= -->
+    <section id="quienes-somos">
+        <div class="text-center">
+            <h1 class="section-header" data-aos="fade-up" data-aos-delay="200">
+                <span class="underline">&nbsp;&nbsp;¿QUIÉNES SOMOS?&nbsp;&nbsp;</span>
+            </h1>
+            <p class="subtitle" data-aos="fade-up" data-aos-delay="400">
+                Somos un grupo de psicólogos con experiencia en las áreas de trabajo social, 
+                clínico y de investigación sobre el trabajo y seguimiento de pacientes con 
+                experiencias psiquiátricas quienes tomamos iniciativa frente a la realidad 
+                social en México de las personas que han sido diagnosticadas con padecimientos 
+                psiquiátricos o que han tenido dificultades para reintegrarse a sus actividades 
+                sociales, familiares y ocupacionales después de una crisis. Con ello buscamos 
+                brindar un espacio de integración, interacción y de inclusión en un ambiente 
+                de aceptación y comunidad.
+            </p>
+        </div>
+        <div class="contaifner" data-aos="fade-up" data-aos-delay="600">
+            <div class="d-flex justify-content-center">
                 <div class="carousel slide carrousel-sizing" id="main-carousel" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -49,47 +49,46 @@
                         <span class="sr-only" aria-hidden="true">Next</span>
                     </a>
                 </div><!-- /.carousel -->
-        <div>
-    </div><!-- /.container -->
-</section>
+            <div>
+        </div><!-- /.container -->
+    </section>
 
-<!-- =======  OUR PHILOSOPHY SECTION ======= -->
-<section id="philosophy" class="container">
-    <div class="row justify-content-center">
-        <h1 class="section-header" data-aos="fade-up" data-aos-delay="200">
-            <span class="underline">&nbsp;&nbsp;NUESTRA FILOSOFÍA&nbsp;&nbsp;</span>
-        </h1>
-        <div data-aos="fade-up" data-aos-delay="400" >
-            <p class="subtitle text-center" >
-                En este modelo el paciente vive en una comunidad que les permite establecer puentes 
-                de interacción de manera controlada y segura, y aprender de manera colaborativa con 
-                el objetivo de brindarle herramientas para su autonomía a través de un proceso 
-                personal que le permita hacerse responsable de sus acciones.
-            </p>
-            <p class="subtitle text-center">
-                Por lo tanto, quienes integran la comunidad terapéutica tenemos la firme convicción de que el
-                trabajo dirigido y centrado en el paciente, debe ser interdisciplinario e integral para el
-                beneficio de éste y su familia. 
-            </p>
-            <p class="subtitle text-center">
-                Al ser un programa integral, contamos con talleres dirigidos para el huésped, que le permiten
-                revitalizar sus habilidades cognitivas, psicomotrices, intelectuales, afectivas y sociales,
-                lo que deriva en una reinserción social total.
-            </p>
+    <!-- =======  OUR PHILOSOPHY SECTION ======= -->
+    <section id="philosophy">
+        <div class="text-center">
+            <h1 class="section-header" data-aos="fade-up" data-aos-delay="200">
+                <span class="underline">&nbsp;&nbsp;NUESTRA FILOSOFÍA&nbsp;&nbsp;</span>
+            </h1>
+            <div data-aos="fade-up" data-aos-delay="400" >
+                <p class="subtitle text-center" >
+                    En este modelo el paciente vive en una comunidad que les permite establecer puentes 
+                    de interacción de manera controlada y segura, y aprender de manera colaborativa con 
+                    el objetivo de brindarle herramientas para su autonomía a través de un proceso 
+                    personal que le permita hacerse responsable de sus acciones.
+                </p>
+                <p class="subtitle text-center">
+                    Por lo tanto, quienes integran la comunidad terapéutica tenemos la firme convicción de que el
+                    trabajo dirigido y centrado en el paciente, debe ser interdisciplinario e integral para el
+                    beneficio de éste y su familia. 
+                </p>
+                <p class="subtitle text-center">
+                    Al ser un programa integral, contamos con talleres dirigidos para el huésped, que le permiten
+                    revitalizar sus habilidades cognitivas, psicomotrices, intelectuales, afectivas y sociales,
+                    lo que deriva en una reinserción social total.
+                </p>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- =======  OUR HISTORY SECTION ======= -->
-<section id="history" class="container">
-    <div class="row justify-content-center">
-        <h1 class="section-header" data-aos="fade-up" data-aos-delay="200">
+    <!-- =======  OUR HISTORY SECTION ======= -->
+    <section id="history">
+        <h1 class="section-header text-center mb-3" data-aos="fade-up" data-aos-delay="200">
             <span class="underline">&nbsp;&nbsp;NUESTRA HISTORIA&nbsp;&nbsp;</span>
         </h1>
         <div class="row">
             <div class="col-md-8" data-aos="fade-left" data-aos-delay="400">
                 <h2 class="subheader text-left">
-                    <span>&nbsp;&nbsp;¿De dónde surge nuestro nombre?&nbsp;&nbsp;</span>
+                    <span>¿De dónde surge nuestro nombre?</span>
                 </h2>
                 <p>Casa Loohl, recibe su nombre por su significado en maya que quiere decir “Flor”. Retomamos
                             nuestras raíces mexicanas, pero sobre todo, teniendo en cuenta que el eje de nuestro trabajo es
@@ -105,7 +104,7 @@
         <div class="row">
             <div class="col" data-aos="fade-left" data-aos-delay="200">
                 <h2 class="subheader text-left">
-                    <span>&nbsp;&nbsp;¿Cómo surge Casa Loohl?&nbsp;&nbsp;</span>
+                    <span>¿Cómo surge Casa Loohl?</span>
                 </h2>
                 <p>Loohl, surge como iniciativa de un grupo de colegas psicólogos con experiencia en las áreas de
                             trabajo social, clínico y de investigación sobre pacientes con enfermedades mentales a partir de
@@ -116,7 +115,11 @@
         <div class="row justify-content-center" data-aos="fade-left" data-aos-delay="500">
             <img class="d-block img-fluid wide" src="/about_us/test_image_4.jpeg" alt="">
         </div>
-    </div>
-</section>
+    </section>
 
+</div>
 @endsection
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('/css/about_us.css') }}">
+@endpush
