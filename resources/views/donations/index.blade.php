c1cb482a961aa5a63f6c1d31c4b97fb2c38c2d7f
angie_anaya <angianaya17@gmail.com>
2021-11-04 11:10:56 -0600

    refactor(donacion): change whole structure of donation view and add styles

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -6,49 +6,15 @@
     <div class="container margin-login">
 
         <section id="contact" class="contact">
-
             <div class="container" data-aos="fade-up">
-
-                <header class="section-header">
-                    <p>¡Apoyanos!</p>
-                </header>
-                <br><br>
-                <p>Si deseas también puedes apoyarnos con donativos en especie, puedes traerlo a nuestras oficinas --DIRECCION-- -HORARIOS-- </p>
-                <br><br>
+                <h1 class="section-header text-center" data-aos="fade-up" data-aos-delay="200">
+                    <span class="underline">&nbsp;&nbsp;¡APÓYANOS!&nbsp;&nbsp;</span>
+                </h1>
+                <p class="subtitle text-center" data-aos="fade-up" data-aos-delay="400">
+                Nosotros creemos que todas las personas son capaces y optamos por hacer una diferencia en y desde lo singular de cada uno de nuestros huéspedes. Gracias a su apoyo, confianza y fe en Casa Loohl el impacto positivo en la comunidad crece de manera exponencial, pues nos permite traer a la vida proyectos impresionantes.
+                </p>
                 <div class="row gy-4 text-center">
-                    <div class="col-lg-8">
-                        <div class="row gy-4">
-                            <div class="col-md-6">
-                                <div class="info-box">
-                                    <i class="bi bi-geo-alt"></i>
-                                    <h3>Dirección</h3>
-                                    <p>A108 Adam Street,<br>New York, NY 535022</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="info-box">
-                                    <i class="bi bi-telephone"></i>
-                                    <h3>LLámanos</h3>
-                                    <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="info-box">
-                                    <i class="bi bi-envelope"></i>
-                                    <h3>Mandanos un Email</h3>
-                                    <p>info@example.com<br>contact@example.com</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="info-box">
-                                    <i class="bi bi-clock"></i>
-                                    <h3>Horarios </h3>
-                                    <p>Lunes - Viernes<br>9:00AM - 05:00PM</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-12">
                         <div class="card my-card shadow">
                             <div class="card-body">
                                 @if (session('status'))
@@ -61,9 +27,12 @@
                                 @endif
                                 <form action="{{route('donation.pay')}}" method="post" id="paymentForm">
                                     @csrf
-                                    <div class="row text-center">
-                                        <div class="col-auto">
-                                            <label>¡Toda donación cuenta!</label>
+                                    <h2 class="subheader text-center">
+                                        <span>¡Toda donación cuenta!</span>
+                                    </h2>
+                                    <div class="row">
+                                        <div class="col-md-6 text-left">
+                                            <label>Monto:</label>
                                             <input
                                                 type="number"
                                                 name="value"
@@ -76,8 +45,8 @@
                                             <small class="form-text text-muted">Utilice valores con decimales separados
                                                 por puntos "."</small>
                                         </div>
-                                        <div class="col-auto">
-                                            <label>Moneda</label>
+                                        <div class="col-md-6 text-left">
+                                            <label>Moneda:</label>
                                             <select class="custom-select" name="currency" required>
                                                 @foreach($currencies as $currency)
                                                     <option value="{{$currency->iso}}"
@@ -89,11 +58,10 @@
                                     <br><br>
                                     <input type="hidden" value="1" name="payment_plataform">
                                     <!--Aqui se pone estatico el valor ID de la plataforma paypal, en caso de tener mas, se sustituye por un valor dinamico-->
-                                    <div class="text-left mt-3">
+                                    <div class="text-center mt-3">
                                         <button type="submit" id="payButton"
-                                                class="btn btn-primary btn-lg paypal-button">Dona con PayPal <img
-                                                src="{{asset('images/payment-plataforms/icon-paypal.png')}}"
-                                                alt="PayPal" class="paypal-icon"></button>
+                                                class="btn btn-read-more d-inline-flex align-items-center justify-content-center align-self-center paypal-button">Donar con PayPal <i class="bi bi-paypal"></i>
+                                        </button>
                                     </div>
                                 </form>
                             </div>
@@ -102,10 +70,5 @@
                 </div>
             </div>
         </section><!-- End Contact Section -->
-    </div>
-    <div class="container-fluid">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59750.53047354275!2d-100.44523666388083!3d20.61221376107346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35b8fdc5b9255%3A0x97b094aa561b832f!2sSantiago%20de%20Quer%C3%A9taro%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1635339969368!5m2!1ses-419!2smx"
-            width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 @endsection
