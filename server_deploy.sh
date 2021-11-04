5d179ed878df50df71cecc7c446c9f9be386885b
JoseCarlosPa <josecarlospas1@gmail.com>
2021-11-04 08:44:03 -0600

    setup: Se modifica archivos de CD

--- server_deploy.sh
+++ server_deploy.sh
@@ -10,6 +10,11 @@ echo "Deploying application ..."
 php artisan down || true
     # Update codebase
     git pull origin main
+    php artisan composer install --no-dev
+    php artisan config:clear
+    php artisan config:cache
+    php artisan route:clear
+    php artisan route:cache
 # Exit maintenance mode
 php artisan up
 
