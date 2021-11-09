1c4bb0f33ba68bf8f4474bb1c0aba6981128d77d
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-11-08 18:19:44 -0700

    fix(hosts): button tooltips

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -9,11 +9,12 @@
 @section('content')
     <div class="row">
         <div class="col-md-6 text-left">
-        <h1 class="section-header mt-5 mb-5"> 
-            <span class="underline">&nbsp;&nbsp;HUÉSPEDES&nbsp;&nbsp;</span>
-        </h1>
+            <h1 class="section-header mt-5 mb-5"> 
+                <span class="underline">&nbsp;&nbsp;HUÉSPEDES&nbsp;&nbsp;</span>
+            </h1>
         </div>
         <div class="col-md-6 text-right mt-5">
+            <!-- Register new guest button -->
             <button class="btn btn-principal d-inline-flex align-items-center justify-content-center align-self-center" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 Registrar huésped
                 <!--<i class="bi bi-plus-circle-fill" style="font-size: 20px;"></i>-->
@@ -21,18 +22,19 @@
         </div>
     </div>
 
-    <!-- Modal -->
+    <!-- Register new guest modal -->
     <div class="modal fade" id="staticBackdrop"  tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                <div class="row col-md-12 justify-content-center text-center">
-                                <h2 class="section-header modal-title" id="staticBackdropLabel"> 
-            <span class="underline text-uppercase">&nbsp;&nbsp;REGISTRAR HUÉSPED&nbsp;&nbsp;</span>
-        </h2></div>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
+                    <div class="row col-md-12 justify-content-center text-center">
+                        <h2 class="section-header modal-title" id="staticBackdropLabel"> 
+                            <span class="underline text-uppercase">&nbsp;&nbsp;REGISTRAR HUÉSPED&nbsp;&nbsp;</span>
+                        </h2>
+                    </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
                     <form method="POST" action="{{route('patients.store')}}" enctype="multipart/form-data">
@@ -55,16 +57,15 @@
         </div>
     </div>
 
-    <br>
     <div class="row">
         <div class="col-md-12">
             <table id="table_id" class="display table-striped">
                 <thead>
-                <tr>
-                    <th class="text-center">ID</th>
-                    <th>Nombre</th>
-                    <th>Acciones</th>
-                </tr>
+                    <tr>
+                        <th class="text-center">ID</th>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
+                    </tr>
                 </thead>
                 <tbody>
                 @foreach($pacientes as $paciente)
@@ -72,12 +73,14 @@
                         <td class="text-center">{{$paciente->id}}</td>
                         <td>{{$paciente->name}}</td>
                         <td>
-                            <button class="btn btn-info" type="button" data-tooltip="tooltip" data-placement="right" title="Eliminar el taller.">Ver mas</button>
-                            <a href="#">
-                                <button class="btn btn-warning" type="button">Editar</button>
+                            <button class="btn btn-info tooltipped-btn" type="button" data-tooltip="tooltip" data-placement="right" title="Ver el detalle del huésped">
+                                <i class="bi bi-eye-fill"></i>
+                            </button>
+                            <a class="btn btn-warning tooltipped-btn" href="" data-tooltip="tooltip" data-placement="right" title="Modificar el huésped" role="button">
+                                <i class="bi bi-pencil-fill"></i>
                             </a>
-                            <button type="button" class="btn btn-danger" data-toggle="modal"
-                                    data-target="#eliminar-{{$paciente->id}}">Eliminar
+                            <button type="button" class="btn btn-danger tooltipped-btn" data-toggle="modal" data-target="#eliminar-{{$paciente->id}}" title="Eliminar el huésped">
+                                <i class="bi bi-trash-fill"></i>
                             </button>
                         </td>
                     </tr>
@@ -117,6 +120,7 @@
         </div>
     </div>
 @endsection
+
 @push('scripts')
     <script
         src="https://code.jquery.com/jquery-3.6.0.min.js"
