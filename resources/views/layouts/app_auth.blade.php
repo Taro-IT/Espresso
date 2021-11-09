ec378acb301eaeec8511548e034ab407d17e23bc
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-11-08 20:22:48 -0700

    fix(administration) navbar transition

--- resources/views/layouts/app_auth.blade.php
+++ resources/views/layouts/app_auth.blade.php
@@ -150,6 +150,7 @@
 </body>
 
 <!-- Vendor JS Files -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="{{asset('vendor')}}/bootstrap-4.6.0/js/bootstrap_2.bundle.js"></script>
 
 <!-- Custom JS files -->
