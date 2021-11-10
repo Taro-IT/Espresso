88f7db29261186158b6d347a85d20482d010382b
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-11-09 19:59:58 -0700

    fix(info) spelling of one word

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@
                 <li class="dropdown"><a class="nav-link scrollto" href="{{route('index')}}#contribute"><span>INVOLÚCRATE</span><i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="{{route('tienda')}}">TIENDA EN LINEA</a></li>
-                        <li><a href="{{route('puntos-venta')}}">TIENDAS FISICAS</a></li>
+                        <li><a href="{{route('puntos-venta')}}">TIENDAS FÍSICAS</a></li>
                     </ul>
                 </li>
                 <li><a class="nav-link" href="/#contact">CONTÁCTANOS</a></li>
