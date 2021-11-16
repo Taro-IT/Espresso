95e69494c3ccf2a0423576c00e032158751c386e
Jose Carlos <A01702828@itesm.mx>
2021-11-16 11:16:21 -0600

    feat: Se agregan test CRDU  Completos para Productos

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\products;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -34,5 +35,45 @@ class ProductsTest extends TestCase
         ]);
 
         $response->assertRedirect('/productos');
+    }
+
+    public function test_eliminar_producto()
+    {
+        $user = User::factory(User::class)->make();
+        $response = $this->actingAs($user)->post('/almacenar-productos', [
+            'id_patient' => 1,
+            'id_workshop' => 1,
+            'title' => 'Test',
+            'description' => 'test',
+            'price' => 208000
+        ]);
+
+        $lastProduct = products::get()->last();
+        $response = $this->actingAs($user)->delete('/productos-eliminar/'.$lastProduct->id);
+        $response->assertRedirect('/productos');
+
+
+    }
+
+    public function test_editar_producto()
+    {
+        $user = User::factory(User::class)->make();
+        $response = $this->actingAs($user)->post('/almacenar-productos', [
+            'id_patient' => 1,
+            'id_workshop' => 1,
+            'title' => 'Test',
+            'description' => 'test',
+            'price' => 208000
+        ]);
+
+        $lastProduct = products::get()->last();
+        $response = $this->actingAs($user)->put('/actualizar-productos/'.$lastProduct->id,[
+            'title' => 'Test Edit',
+            'description' => 'test Edit',
+            'price' => 0
+        ]);
+        $response->assertRedirect('/productos');
+
+
     }
 }
